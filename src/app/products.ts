b254188100adc9843173957aa7b69c6a054fe964
tommyenduro <tommyenduro@gmail.com>
2022-04-06 16:30:44 -0400

    4 files modified, 4 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -23,6 +23,12 @@ export const products = [
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    id: 4,
+    name: 'Phone SE2',
+    price: 400,
+    description: 'An affordable competent phone'
   }
 ];
 
